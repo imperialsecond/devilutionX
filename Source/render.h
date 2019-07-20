c81e6e964b4b2dd2a040f898751ccddea1c582b7
imperialsecond <imperialsecond@users.noreply.github.com>
2019-07-20 14:09:38 +0200

    Remove dead code.

--- Source/render.h
+++ Source/render.h
@@ -12,14 +12,5 @@ void world_draw_black_tile(BYTE *pBuff);
 /* rdata */
 
 extern BYTE *gpBufStart;
-extern int WorldBoolFlag;
-extern unsigned int gdwCurrentMask;
-extern unsigned int *gpDrawMask;
-extern unsigned int RightMask[32];
-extern unsigned int LeftMask[32];
-extern unsigned int WallMask[32];
-extern int WorldTbl3x16[48];
-extern int WorldTbl17_1[17];
-extern int WorldTbl17_2[17];
 
 #endif /* __RENDER_H__ */
